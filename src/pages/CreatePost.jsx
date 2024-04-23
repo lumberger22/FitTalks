ef739110c05e86ffc4fafdc8c706d039de9ada02
lucasvt22 <lucas22@vt.edu>
2024-04-23 19:25:27 -0400

    Add functionality to create post

---
+++ src/pages/CreatePost.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { useState } from 'react'
+import { supabase } from '../client'
+import './CreatePost.css'
+
+export default function CreatePost() {
+
+    const [post, setPost] = useState({title: "", description: "", media: "", likes: 0})
+
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setPost( (prev) => {
+            return {
+                ...prev,
+                [name]:value,
+            }
+        })
+    }
+
+    const createPost = async (event) => {
+        event.preventDefault();
+      
+        await supabase
+          .from('Posts')
+          .insert({title: post.title, description: post.description, media: post.media, likes: post.likes})
+          .select();
+      
+        window.location = "/posts";
+    }
+
+    return (
+        <div>
+            <form autoComplete='off'>
+                <input 
+                    type="text" 
+                    id="title" 
+                    name="title" 
+                    placeholder="Title" 
+                    required 
+                    onChange={handleChange} /><br />
+                <br/>
+
+                <input
+                    type="text" 
+                    id="description" 
+                    name="description" 
+                    placeholder="Content (Optional)" 
+                    onChange={handleChange} /><br />
+                <br/>
+
+                <input
+                    type="text" 
+                    id="media" 
+                    name="media" 
+                    placeholder="Image URL (Optional)"
+                    onChange={handleChange} /><br />
+                <br/>
+                
+                <input type="submit" value="Create Post" onClick={createPost}/>
+            </form>
+        </div>
+    )
+}
