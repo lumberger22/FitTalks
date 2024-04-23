ec649a8c9071be2e78307f97ac52748f4cd6d5b9
lucasvt22 <lucas22@vt.edu>
2024-04-23 19:52:11 -0400

    Create post component and link to gallery

---
+++ src/Components/Post.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useState } from 'react'
+import './Post.css'
+import { Link } from 'react-router-dom'
+
+
+const Post = (props) =>  {
+
+  return (
+      <div className="post">
+        <div className='post--content'>
+            <p className='post--timestamp'>{props.timestamp}</p> 
+          <Link to={'detailPage/' + props.id}>
+            <h3 className='post--title'>{props.title}</h3>
+          </Link>
+          <p className='post--likes'>{props.likes}</p>
+          <Link to={'edit/'+ props.id}>
+            <img className="more-btn" alt="edit button" src='#' />
+          </Link>
+        </div>
+      </div>
+  );
+};
+
+export default Card;
