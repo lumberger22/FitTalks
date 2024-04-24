56e1883d6dce027b82156ea76fc12f39746c6843
lucasvt22 <lucas22@vt.edu>
2024-04-23 20:04:01 -0400

    Add links to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import React from 'react';
+import { useRoutes } from 'react-router-dom'
+import Gallery from './pages/Gallery'
+import Home from './pages/Home'
+import CreatePost from './pages/CreatePost'
+import EditPost from './pages/EditPost'
+import DetailPage from './pages/DetailPage'
+import { Link } from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+const App = () => { 
+
+  // Sets up routes
+  let element = useRoutes([
+    {
+      path: "/",
+      element:<Home />
+    },
+    {
+      path: "/posts",
+      element:<Gallery />
+    },
+    {
+      path:"/posts/editPost/:id",
+      element: <EditPost />
+    },
+    {
+      path:"/newPost",
+      element: <CreatePost />
+    },
+    {
+      path:"/posts/detailPage/:id",
+      element: <DetailPage />
+    }
+  ]);
+
+  return ( 
+
+    <div className="App">
+      <div className="nav">
+        <h1>FitTalks</h1>
+        <Link to="/"><button className="navBtn"> Home </button></Link>
+        <Link to="/posts"><button className="navBtn"> Posts </button></Link>
+        <Link to="/newPost"><button className="navBtn"> Create Post </button></Link>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='content'>
+        {element}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+
+  );
 }
 
-export default App
+export default App;
