1f5708a5c061f56864e49e10b9c2db2e6ba35a63
lucasvt22 <lucas22@vt.edu>
2024-04-25 01:07:23 -0400

    Add timeago import and incorperate into post

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState } from 'react'
 import './Post.css'
 import { Link } from 'react-router-dom'
+import TimeAgo from './Timeago'
 
 
 const Post = (props) =>  {
@@ -9,12 +10,12 @@ const Post = (props) =>  {
   return (
       <div className="post">
         <div className='post--content'>
-            <p className='post--timestamp'>{props.timestamp}</p> 
+          <TimeAgo timestamp={props.timestamp} />
           <Link to={'detailPage/' + props.id}>
             <h3 className='post--title'>{props.title}</h3>
           </Link>
           <p className='post--likes'>{props.likes}</p>
-          <Link to={'edit/'+ props.id}>
+          <Link to={'editPost/'+ props.id}>
             <img className="more-btn" alt="edit button" src='#' />
           </Link>
         </div>
